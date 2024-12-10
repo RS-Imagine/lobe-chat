3af7455ce95c302eed0ae26c0ae58789d5edc400
Qiulin <20410040207@stu.xust.edu.cn>
2024-12-10 09:43:29 +0800

    Change DEFAULT (branding and llm)

--- src/const/branding.ts
+++ src/const/branding.ts
@@ -4,8 +4,8 @@
 
 export const LOBE_CHAT_CLOUD = 'LobeChat Cloud';
 
-export const BRANDING_NAME = 'LobeChat';
-export const BRANDING_LOGO_URL = '';
+export const BRANDING_NAME = 'ImagineChat';
+export const BRANDING_LOGO_URL = 'https://avatars.githubusercontent.com/u/110177673?v=4&size=64';
 
 export const ORG_NAME = 'LobeHub';
 
