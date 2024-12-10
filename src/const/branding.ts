62ac5ccd2994e7ece7707a917de21a8d4d730ef4
Qiulin <20410040207@stu.xust.edu.cn>
2024-12-10 15:23:10 +0800

    change BRANDING_LOGO_URL

--- src/const/branding.ts
+++ src/const/branding.ts
@@ -5,7 +5,7 @@
 export const LOBE_CHAT_CLOUD = 'LobeChat Cloud';
 
 export const BRANDING_NAME = 'ImagineChat';
-export const BRANDING_LOGO_URL = '';
+export const BRANDING_LOGO_URL = 'https://pic.imgdb.cn/item/6757ec44d0e0a243d4e0fabc.png';
 
 export const ORG_NAME = 'LobeHub';
 
