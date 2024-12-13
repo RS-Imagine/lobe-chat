eaabd4e7a813cdad915d041b24ca4cd9c4db25cd
Qiulin <20410040207@stu.xust.edu.cn>
2024-12-13 11:04:54 +0800

    该分支仅和plugin 的homepage有关

--- src/app/(backend)/webapi/tts/edge/route.ts
+++ src/app/(backend)/webapi/tts/edge/route.ts
@@ -1,9 +1,5 @@
 import { EdgeSpeechPayload, EdgeSpeechTTS } from '@lobehub/tts';
 
-// 设置全局 HTTPS 代理
-process.env.HTTPS_PROXY = 'https://proxy.forimagine.eu.org/proxy/';
-
-
 export const runtime = 'edge';
 
 export const POST = async (req: Request) => {
