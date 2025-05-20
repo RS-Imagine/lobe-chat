b697902085d32968333a0e91e8b448b789ee2b4f
RS-Imagine <20410040207@stu.xust.edu.cn>
2025-05-20 11:01:22 +0800

    版本更新至1.87.4 and 修改网站图标和名称

--- src/const/branding.ts
+++ src/const/branding.ts
@@ -2,15 +2,15 @@
 // if you want to use it in the commercial usage
 // please contact us for more information: hello@lobehub.com
 
-export const LOBE_CHAT_CLOUD = 'ImagineChat';
+export const LOBE_CHAT_CLOUD = 'ImagineAI';
 
 export const BRANDING_NAME = 'Imagine';
-export const BRANDING_LOGO_URL = 'https://pic1.imgdb.cn/item/6807028458cb8da5c8bdc756.jpg';
+export const BRANDING_LOGO_URL = 'https://pic1.imgdb.cn/item/682beff558cb8da5c8fd2dcc.webp';
 
 export const ORG_NAME = 'Imagine';
 
 export const BRANDING_URL = {
   help: 'https://forimagine.eu.org/about',
   privacy: 'https://github.com/RS-Imagine',
-  terms: 'https://forimagine.eu.org/',
+  terms: 'https://forimagine.eu.org',
 };
